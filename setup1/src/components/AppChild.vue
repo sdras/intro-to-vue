6ca4ce6fe61b79983806e71584e55bbd08c05cfc
Sarah Drasner <sarah.drasner@gmail.com>
2020-07-26 16:39:31 -0600

    put in setup1 repo

---
+++ setup1/src/components/AppChild.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item">
+    <h2>{{ item }}</h2>
+    <img :src="url" width="235" height="300" />
+    <div class="quantity">
+      <button class="inc" @click="counter > 0 ? counter -= 1 : 0">-</button>
+      <span class="quant-text">Quantity: {{ counter }}</span>
+      <button class="inc" @click="counter += 1">+</button>
+    </div>
+    <button class="submit">Submit</button>
+  </div>
+  <!--item-->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  props: ["item", "url"],
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
