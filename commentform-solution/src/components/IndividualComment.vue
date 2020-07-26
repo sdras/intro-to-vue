d3f892984e0e182f80c93a47ec3d66c4ed38c3a8
Sarah Drasner <sarah.drasner@gmail.com>
2020-07-26 17:24:19 -0600

    add in commentform solution

---
+++ commentform-solution/src/components/IndividualComment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <li> 
+      <img class="post-img" :src="commentpost.authorImg" /> 
+      <small>{{ commentpost.author }}</small>
+      <p class="post-comment">"{{ commentpost.text }}"</p>
+    </li>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    props: ['commentpost']
+  }
+</script>
---
+++ commentform-solution/src/store/store.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    comments: [
+      { 
+        text: 'Looks great Julianne!',
+        author: 'Robin Rendle',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg'
+      },
+      { 
+        text: 'I love the Sea',
+        author: 'Miriam Suzanne',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg'
+      },
+      { 
+        text: 'Where are you?',
+        author: 'Geoff Graham',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
+      }
+    ]
+  },
+  mutations: {
+    addComment(state, comment) {
+      const newCommentObj = {
+        text: comment,
+        author: 'Magoo',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-skull.jpg'
+      };
+      state.comments.push(newCommentObj);
+    }
+  }
+});
